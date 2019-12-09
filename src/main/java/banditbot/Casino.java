4a09695f542cd5445a8ff491d4fcab61c21dbe97
jerseybiomed <panov1213@gmail.com>
2019-12-09 12:29:46 +0500

    Add command '/balance'

--- src/main/java/banditbot/Casino.java
+++ src/main/java/banditbot/Casino.java
@@ -2,6 +2,7 @@ package banditbot;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import bot.Bot;
@@ -22,6 +23,7 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 public class Casino extends Bot {
     private Drum drum;
     private Message message;
+    private HashMap<Long, Double> balances = new HashMap<>();
     private ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
 
     public Casino(Drum drum, String userName, String token) {
@@ -29,6 +31,7 @@ public class Casino extends Bot {
         this.drum = drum;
         this.setReplyKeyboardMarkup();
         ECommands.Roll.sendTo(this.commands::replace, this::roll);
+        ECommands.Balance.sendTo(this.commands::replace, (args) -> balances.get(message.getChatId()));
         ECommands.Help.sendTo(this.commands::replace, (args) -> sendMessage(message, Help.help));
         ECommands.Lines.sendTo(this.commands::add, (args) -> sendMessage(message, Help.lines));
         ECommands.Start.sendTo(this.commands::add, (args) -> sendMessage(message, Help.help));
@@ -36,7 +39,9 @@ public class Casino extends Bot {
 
     private void roll(String[] args) {
         try {
-            double res = drum.roll(Integer.parseInt(args[1]));
+            int bet = Integer.parseInt(args[1]);
+            double res = drum.roll(bet);
+            balances.replace(message.getChatId(), balances.get(message.getChatId()) - bet + res);
             String message = "line:" + drum.getComb() + " result:" + res;
             sendMessage(this.message, message);
         } catch (IOException e) {
@@ -49,7 +54,10 @@ public class Casino extends Bot {
         if (update.hasMessage()) {
             message = update.getMessage();
             try {
-                this.perform(message.getText().split(" "));
+                String[] args = message.getText().split(" ");
+                if (args[0].equals("/start"))
+                    balances.put(message.getChatId(), 10000.0);
+                this.perform(args);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -74,8 +82,11 @@ public class Casino extends Bot {
         KeyboardRow keyboardFirstRow = new KeyboardRow();
         keyboardFirstRow.add(new KeyboardButton("/help"));
         keyboardFirstRow.add(new KeyboardButton("/lines"));
+        keyboardFirstRow.add(new KeyboardButton("/balance"));
         KeyboardRow keyboardSecondRow = new KeyboardRow();
+        keyboardSecondRow.add(new KeyboardButton("/roll 10"));
         keyboardSecondRow.add(new KeyboardButton("/roll 100"));
+        keyboardSecondRow.add(new KeyboardButton("/roll 1000"));
         keyboard.add(keyboardFirstRow);
         keyboard.add(keyboardSecondRow);
         replyKeyboardMarkup.setKeyboard(keyboard);
