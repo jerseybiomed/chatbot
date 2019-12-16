2e6b3ae24af2f76a179adda92a5fe3624ca089b1
jerseybiomed <panov1213@gmail.com>
2019-12-16 18:19:42 +0500

    update Tests

--- src/test/java/web/RandomizeTest.java
+++ src/test/java/web/RandomizeTest.java
@@ -7,35 +7,36 @@ import java.io.IOException;
 import org.junit.jupiter.api.Test;
 
 class RandomizeTest {
+	Randomize randomize = new Randomize();
 
 	@Test
 	public void rangeTestFirst() throws IOException {
 		String request = "https://www.random.org/integers/?num=1&min=1&max=2&col=1&base=10&format=plain&rnd=new";
-		assertTrue(1 <= Randomize.GetRandomNumber(request) && Randomize.GetRandomNumber(request) <= 2);
+		assertTrue(1 <= randomize.Next(request) && randomize.Next(request) <= 2);
 	}
 	
 	@Test
 	public void rangeTestSecond() throws IOException {
 		String request = "https://www.random.org/integers/?num=1&min=1&max=5&col=1&base=10&format=plain&rnd=new";
-		assertTrue(1 <= Randomize.GetRandomNumber(request) && Randomize.GetRandomNumber(request) <= 5);
+		assertTrue(1 <= randomize.Next(request) && randomize.Next(request) <= 5);
 	}
 	
 	@Test
 	public void rangeTestThird() throws IOException {
 		String request = "https://www.random.org/integers/?num=1&min=5&max=10&col=1&base=10&format=plain&rnd=new";
-		assertTrue(5 <= Randomize.GetRandomNumber(request) && Randomize.GetRandomNumber(request) <= 10);
+		assertTrue(5 <= randomize.Next(request) && randomize.Next(request) <= 10);
 	}
 	
 	@Test
 	public void rangeTestFourth() throws IOException {
 		String request = "https://www.random.org/integers/?num=1&min=0&max=25&col=1&base=10&format=plain&rnd=new";
-		assertTrue(0 <= Randomize.GetRandomNumber(request) && Randomize.GetRandomNumber(request) <= 25);
+		assertTrue(0 <= randomize.Next(request) && randomize.Next(request) <= 25);
 	}
 	
 	@Test
 	public void rangeTestFifth() throws IOException {
 		String request = "https://www.random.org/integers/?num=1&min=1&max=50&col=1&base=10&format=plain&rnd=new";
-		assertTrue(1 <= Randomize.GetRandomNumber(request) && Randomize.GetRandomNumber(request) <= 50);
+		assertTrue(1 <= randomize.Next(request) && randomize.Next(request) <= 50);
 	}
 
 }
