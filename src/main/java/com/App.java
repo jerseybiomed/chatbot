668e41b66d11980c6d0f409dacea0bc8cba96341
Slave1488 <blue101blower@gmail.com>
2019-12-25 16:33:35 +0500

    ~

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,6 +1,8 @@
 package com;
 
-import java.util.Scanner;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 import bot.ChatBot;
 import bot.TelegramBot;
@@ -11,9 +13,6 @@ import games.menu.MenuFabric;
 import games.roulette.Roulette;
 import games.roulette.RouletteFabric;
 import logic.console.ConsoleSender;
-import org.telegram.telegrambots.ApiContextInitializer;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import random.Randomize;
 
 /**
@@ -21,14 +20,14 @@ import random.Randomize;
  */
 public final class App {
     private static void telegramLaunch() {
-        ApiContextInitializer.init();
-        TelegramBotsApi botsApi = new TelegramBotsApi();
         ChatBot bot = new ChatBot(new MenuFabric(
             new BanditFabric(new Randomize()),
             new RouletteFabric(new Roulette(new Randomize()))));
         TelegramBot telegram = new TelegramBot(System.getenv("BOT_USERNAME"), System.getenv("BOT_TOKEN"));
         TelegramDispatcher dispatcher = new TelegramDispatcher(telegram, bot);
         telegram.pub(dispatcher);
+        ApiContextInitializer.init();
+        TelegramBotsApi botsApi = new TelegramBotsApi();
         try {
             botsApi.registerBot(telegram);
         } catch (TelegramApiRequestException e) {
@@ -46,6 +45,7 @@ public final class App {
         }
     }
         
-    void main(final String[] args) {
+    public static void main(final String[] args) {
+        telegramLaunch();
     }
 }
